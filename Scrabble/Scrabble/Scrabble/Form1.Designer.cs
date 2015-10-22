0e914b023c87088eb4e3c5e86350c492bcf8ceb6
sh4ne <shane.andrus@aggiemail.usu.edu>
2015-10-22 12:39:53 -0600

    another change

--- Scrabble/Scrabble/Scrabble/Form1.Designer.cs
+++ Scrabble/Scrabble/Scrabble/Form1.Designer.cs
@@ -29,20 +29,30 @@
         private void InitializeComponent()
         {
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // monthCalendar1
             // 
             this.monthCalendar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.monthCalendar1.Location = new System.Drawing.Point(994, 42);
+            this.monthCalendar1.Location = new System.Drawing.Point(357, 42);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(333, 425);
+            this.webBrowser1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1272, 397);
+            this.ClientSize = new System.Drawing.Size(635, 449);
+            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.monthCalendar1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -53,6 +63,7 @@
         #endregion
 
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
