d8709b62b965abe522e8a175457b2be7c4235781
murple <mike.larsen@aggiemail.usu.edu>
2015-10-28 09:19:44 -0600

    Added context menu

--- Scrabble/Form1.Designer.cs
+++ Scrabble/Form1.Designer.cs
@@ -28,20 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mikeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mikeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // mikeToolStripMenuItem
+            // 
+            this.mikeToolStripMenuItem.Name = "mikeToolStripMenuItem";
+            this.mikeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mikeToolStripMenuItem.Text = "mike";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 449);
+            this.ClientSize = new System.Drawing.Size(476, 365);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mikeToolStripMenuItem;
     }
 }
 
