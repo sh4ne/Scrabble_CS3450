57612c027d2e0201238ea9f70e77e14e863fbe56
Joshua Charles Vaughan <joshcv2112@gmail.com>
2015-10-28 13:02:33 -0600

    Member variables and constructors created.

--- Scrabble/Player.cs
+++ Scrabble/Player.cs
@@ -4,9 +4,15 @@ public class Player
 {
     // Player class member variables
     private string name;
+    private LetterRack letterRack;
+    private int playerID;
+    private int skipCount;
+    private int score;
+    private bool hasTurnPriority;
 
-    public Player()
+    public Player(int userID, string username)
     {
-
+        playerID = userID;
+        name = username;
     }
 }
