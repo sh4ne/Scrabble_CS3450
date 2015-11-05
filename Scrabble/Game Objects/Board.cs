7e6591bb16cdc0e0a60b2ecdce800f91fe5f7a34
murple <mike.larsen@aggiemail.usu.edu>
2015-11-04 18:21:24 -0700

    Created Board class. It relies on creation of Play class.

--- Scrabble/Game Objects/Board.cs
+++ Scrabble/Game Objects/Board.cs
@@ -5,8 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Scrabble.Game_Objects
-{
+{  
+    // Board class handles the list of tiles, and plays made to the board.
+
     class Board
     {
+        //Contains the list of board tiles.
+        private List<GameBoardSquare> SquaresList;
+        public Board() { }
+        /// <summary>
+        /// Applies a Play to the tiles in SquaresList.
+        /// Play parameter might need to be traced to its location once created.
+        /// </summary>
+        /// <param name="play">Receives a play and applys that to tiles.</param>
+        public void AddPlayToBoard(Play play) { }
     }
 }
