68876cf4c1d5b63197571add06dcc4e758d86353
sh4ne <shane.andrus@aggiemail.usu.edu>
2015-11-11 11:27:12 -0700

    Everything is in working condition.

--- Scrabble/Form1.cs
+++ Scrabble/Form1.cs
@@ -8,19 +8,116 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using EasyNetwork;
+
 namespace Scrabble
 {
     public partial class Form1 : Form
     {
+        EasyNetwork.Client client = new EasyNetwork.Client("tcp://localhost:9001");
+
+        public class tester
+        {
+            public tester(string s)
+            {
+                this.tosend = s;
+            }
+
+            public string tosend;
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private Scrabble.Logger logger;
+
         private void button1_Click(object sender, EventArgs e)
         {
-            Scrabble.Logger log = new Scrabble.Logger();
-            log.logMessage("HELLO");
+            logger = new Scrabble.Logger();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EasyNetwork.Server server = new Server("tcp://*:9001");
+            server.DataReceived += Server_DataReceived;
+            server.Start();
+            
+        }
+
+        private void Server_DataReceived(object receivedObject, Guid clientId)
+        {
+            try {
+                //process received object
+                if (receivedObject is tester)
+                {
+                    tester clientObj = (tester)receivedObject;
+                    textBox1.Text += ("Look what I got. A tester object that contains the string " + clientObj.tosend.ToString() + ".\n");
+                }
+            }
+            catch(NotImplementedException e)
+            {
+                textBox1.AppendText(e.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string message = textBox2.ToString();
+            tester test = new tester(message);
+            client.Send<tester>(test);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            client.DataReceived += Client_DataReceived;
+            client.Start();
+        }
+
+        private void Client_DataReceived(object receivedObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            logger.logMessage("This is a test message.");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            logger.logWarning("This is a test warning.", "form1 button6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            logger.logError("This is a test error.", "form1 button7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            logger.addToGameState("This goes to gamestate.");
         }
     }
 }
--- Scrabble/Logger.cs
+++ Scrabble/Logger.cs
@@ -6,72 +6,90 @@ using System.Threading.Tasks;
 
 namespace Scrabble
 {
+    /// <summary>
+    /// Logger - creates 2 files which you can write what is happening in the game.
+    /// used for errors, warnings, normal messages, and the change of gamestate.
+    /// </summary>
     class Logger
     {
         /// <summary>
-        /// 
+        /// default constructor - each game world will have a logger to keep track of what is happening in game
         /// </summary>
         public Logger()
         {
-
-            if (System.IO.Directory.Exists(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\"))
-            {
-                this.verboseLog = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "\\Log\\" + string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}", DateTime.Now) + "_VERBOSE_LOG.txt");
-                this.gameStateLog = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}", DateTime.Now) + "_GAME_STATE.txt");
-            }
-            else
-            {
-                System.IO.Directory.CreateDirectory(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\");
-                this.verboseLog = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\" + "VERBOSE_LOG.txt");
-                this.gameStateLog = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\" + "GAME_STATE.txt");
-            }
-            
+            this.now = string.Format("{0:yyyy-MM-dd_hh-mm-ss}", DateTime.Now);
+            System.IO.Directory.CreateDirectory(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\");
         }
 
         /// <summary>
-        /// 
+        /// logMessage - Logs a normal message to the verbose log file.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">This is the message that will be written to file</param>
         public void logMessage(string message)
         {
-            this.verboseLog.WriteLineAsync(message);
+            using (this.verboseLog =
+                   new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\" + this.now + "_VERBOSE_LOG.txt", true))
+            {
+                verboseLog.WriteLineAsync(message);
+            }
+            verboseLog.Close();
         }
 
         /// <summary>
-        /// 
+        /// logWarning - Logs a warning message to the verbose log file.
         /// </summary>
-        /// <param name="warning"></param>
-        public void logWarning(string warning)
+        /// <param name="warning">The warning message that will be written.</param>
+        /// <param name="where">Where the warning is coming from.</param>
+        public void logWarning(string warning, string where)
         {
-
+            using (this.verboseLog =
+                   new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\" + this.now + "_VERBOSE_LOG.txt", true))
+            {
+                verboseLog.WriteLineAsync("*** WARNING - "+where+": "+warning +" ***");
+            }
+            verboseLog.Close();
         }
 
         /// <summary>
-        /// 
+        /// logError - Logs an error message to the verbose log file.
         /// </summary>
-        /// <param name="error"></param>
-        public void logError(string error)
+        /// <param name="error">The error message that will be written.</param>
+        /// <param name="where">Where the error occured.</param>
+        public void logError(string error, string where)
         {
-
+            using (this.verboseLog =
+                   new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\" + this.now + "_VERBOSE_LOG.txt", true))
+            {
+                verboseLog.WriteLineAsync("!!!!!!! ERROR - "+where+": "+error+" !!!!!!!");
+            }
+            verboseLog.Close();
         }
 
         /// <summary>
         /// 
         /// </summary>
-        public void createGameState()
+        /// <param name="toGameState"></param>
+        public void addToGameState(string toGameState)
         {
-
+            using (this.gameStateLog =
+                   new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "\\Logs\\" + this.now + "_GAMESTATE_LOG.txt", true))
+            {
+                gameStateLog.WriteLineAsync(toGameState);
+            }
+            gameStateLog.Close();
         }
 
         /// <summary>
         /// 
         /// </summary>
-        private System.IO.StreamWriter verboseLog;
+        private System.IO.TextWriter verboseLog;
 
         /// <summary>
         /// 
         /// </summary>
-        private System.IO.StreamWriter gameStateLog;
+        private System.IO.TextWriter gameStateLog;
+
+        private string now;
 
     }
 }
