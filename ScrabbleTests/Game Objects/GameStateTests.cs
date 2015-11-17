c38360a3c9f5aeebd9aeedc001d572cd1b09c7dd
sh4ne <shane.andrus@aggiemail.usu.edu>
2015-11-16 23:10:17 -0700

    GameState Created, unit tests also created

---
+++ ScrabbleTests/Game Objects/GameStateTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrabble.Game_Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble.Game_Objects.Tests
+{
+    [TestClass()]
+    public class GameStateTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Logger TempLogger = new Logger();
+            try {
+                    GameState TempState = new GameState();
+                    PlayerClass.Player TempPlayer = new PlayerClass.Player(0001, "username");
+                    TempState.AddPlayer(TempPlayer);
+                    TempLogger.AddToGameState(TempState.ToString());
+                }
+            catch(System.Exception e)
+            {
+                TempLogger.LogError(e.ToString(), "ToStringTest");
+            }
+            Assert.IsFalse(true);
+        }
+    }
+}
