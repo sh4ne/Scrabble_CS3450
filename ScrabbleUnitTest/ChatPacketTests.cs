2f34e0a8d6d9553724cfd2c86f49808c80a57c5c
Peter Scheelke <pas1995@gmail.com>
2015-11-16 20:05:03 -0700

    ChatPacket object, and an empty game world class.

---
+++ ScrabbleUnitTest/ChatPacketTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrabble;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble.Tests
+{
+    [TestClass()]
+    public class ChatPacketTests
+    {
+        [TestMethod()]
+        public void ChatPacketTest()
+        {
+            ChatPacket cp = new ChatPacket(9001, "Blob", "Hello, world . . . or whatever.");
+
+            Assert.IsTrue(cp.PlayerID == 9001);
+            Assert.IsTrue(cp.Username == "Blob");
+            Assert.IsTrue(cp.Message == "Hello, world . . . or whatever.");
+        }
+    }
+}
