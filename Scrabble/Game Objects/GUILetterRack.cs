78e96c9c175caa51ee6c125aca3baf1e44be2c19
murple <mike.larsen@aggiemail.usu.edu>
2015-12-03 13:22:05 -0700

    Added GUI for the board, and a few other things.

---
+++ Scrabble/Game Objects/GUILetterRack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble.GameWorld
+{
+    class GUILetterRack
+    {
+        private List<GUILetterTile> guiLetterTiles;
+        
+        public GUILetterRack(Game_Objects.LetterTileRack letterTileRack)
+        {
+
+        }
+
+        public void update(Game_Objects.LetterTileRack letterTileRack)
+        {
+
+        }
+    }
+}
---
+++ ScrabbleUnitTest/Server/DictionaryTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Scrabble.Server.Tests
+{
+    [TestClass()]
+    public class DictionaryTests
+    {
+        [TestMethod()]
+        public void DictionaryTest()
+        {
+            Assert.IsTrue(System.IO.File.Exists("dictionary.txt"));
+        }
+
+        [TestMethod()]
+        public void containsWordTest()
+        {
+            Dictionary dictionary = new Dictionary("dictionary.txt");
+            Assert.IsTrue(dictionary.containsWord("alphabet"));
+            Assert.IsFalse(dictionary.containsWord("jsjsjsjs"));
+        }
+    }
+}
