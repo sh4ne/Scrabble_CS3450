664c03bbbe52d9d131e42a99329bed84083e03ec
John Johnson <johnsonjo4531@gmail.com>
2015-12-06 15:35:33 -0700

    Adding Play class changes and 100% unit test coverage in Play class.

--- ScrabbleUnitTest/PlayClassTests.cs
+++ ScrabbleUnitTest/PlayClassTests.cs
@@ -24,7 +24,7 @@ namespace ScrabbleUnitTest
         /// Tests the Play class
         /// </summary>
         [TestMethod]
-        public void PlayClassTest()
+        public void ShouldCreateValidPlay()
         {
             // setup
             var letterTileA = new Scrabble.Game_Objects.LetterTile('A', 1);
@@ -32,7 +32,6 @@ namespace ScrabbleUnitTest
             var letterTileT = new Scrabble.Game_Objects.LetterTile('T', 1);
             List<Scrabble.Game_Objects.LetterTile> word = new List<Scrabble.Game_Objects.LetterTile> { letterTileA, letterTileN, letterTileT };
             var coordsX = new List<int> { 0, 1, 2 };
-            var coordsX4 = new List<int> { 3, 2, 1, 0 };
             var coordsY = new List<int> { 0, 0, 0 };
 
             var play = new Scrabble.Play(word, coordsX, coordsY, 1);
@@ -52,7 +51,27 @@ namespace ScrabbleUnitTest
             Assert.AreEqual(letterTileN, play.GetLetterTile(1));
             Assert.AreEqual(letterTileT, play.GetLetterTile(2));
 
-            // should throw VaryingParallelListLength Exception
+            Assert.AreEqual(1, play.GetPlayerID());
+
+            Assert.AreEqual(Scrabble.Play.TileAxis.Horizontal, play.GetTileAxis());
+
+            // switching the coords to get a vertical play
+            var play1 = new Scrabble.Play(word, coordsY, coordsX, 1);
+
+            Assert.AreEqual(Scrabble.Play.TileAxis.Vertical, play1.GetTileAxis());
+        }
+
+        [TestMethod]
+        public void ShouldThrowVaryingParallelListLength()
+        {
+            // setup
+            var letterTileA = new Scrabble.Game_Objects.LetterTile('A', 1);
+            var letterTileN = new Scrabble.Game_Objects.LetterTile('N', 1);
+            var letterTileT = new Scrabble.Game_Objects.LetterTile('T', 1);
+            List<Scrabble.Game_Objects.LetterTile> word = new List<Scrabble.Game_Objects.LetterTile> { letterTileA, letterTileN, letterTileT };
+            var coordsX4 = new List<int> { 3, 2, 1, 0 };
+            var coordsY = new List<int> { 0, 0, 0 };
+
             try
             {
                 var play1 = new Scrabble.Play(word, coordsX4, coordsY, 1);
@@ -60,10 +79,217 @@ namespace ScrabbleUnitTest
                 // should error before this line.
                 Assert.IsTrue(false);
             }
-            catch (Exception err)
+            catch (Scrabble.Play.VaryingParallelListLength err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldThrowEmptyParallelList()
+        {
+            // setup
+            List<Scrabble.Game_Objects.LetterTile> word = new List<Scrabble.Game_Objects.LetterTile> { };
+            var coordsX = new List<int> { };
+            var coordsY = new List<int> { };
+
+            try
+            {
+                var play1 = new Scrabble.Play(word, coordsX, coordsY, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.EmptyParrallelList err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldThrowOutOfBoardBoundaries()
+        {
+            // setup
+            var letterTileA = new Scrabble.Game_Objects.LetterTile('A', 1);
+            var letterTileN = new Scrabble.Game_Objects.LetterTile('N', 1);
+            var letterTileT = new Scrabble.Game_Objects.LetterTile('T', 1);
+            List<Scrabble.Game_Objects.LetterTile> word = new List<Scrabble.Game_Objects.LetterTile> { letterTileA, letterTileN, letterTileT };
+            var coordsX = new List<int> { -1, 0, 1 };
+            var coordsY = new List<int> { 0, 0, 0 };
+
+            try
+            {
+                var play1 = new Scrabble.Play(word, coordsX, coordsY, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.OutOfBoardBoundaries err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldThrowInvalidAxis()
+        {
+            // setup
+            var letterTileA = new Scrabble.Game_Objects.LetterTile('A', 1);
+            var letterTileN = new Scrabble.Game_Objects.LetterTile('N', 1);
+            var letterTileT = new Scrabble.Game_Objects.LetterTile('T', 1);
+            List<Scrabble.Game_Objects.LetterTile> word = new List<Scrabble.Game_Objects.LetterTile> { letterTileA, letterTileN, letterTileT };
+            var incrementingCoordsX = new List<int> { 0, 1, 2 };
+            var invalidCoordsY = new List<int> { 0, 0, 1 };
+            var invalidCoordsX = new List<int> { 0, 0, 1 };
+            var incrementingCoordsY = new List<int> { 0, 1, 2 };
+            var halfIncrementingCoords = new List<int> { 0, 1, 1 };
+            var halfStayingStillCoords = new List<int> { 0, 0, 1 };
+
+            try
+            {
+                var play = new Scrabble.Play(word, incrementingCoordsX, invalidCoordsY, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.InvalidAxis err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                // shouldn't throw another type of error
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                Assert.IsTrue(false);
+            }
+
+            try
+            {
+                var play = new Scrabble.Play(word, invalidCoordsX, incrementingCoordsY, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.InvalidAxis err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
+
+            try
+            {
+                var play = new Scrabble.Play(word, incrementingCoordsX, incrementingCoordsY, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.InvalidAxis err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
+
+            try
+            {
+                var play = new Scrabble.Play(word, halfIncrementingCoords, halfStayingStillCoords, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.InvalidAxis err)
             {
                 Assert.IsTrue(true);
             }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
+
+            try
+            {
+                var play = new Scrabble.Play(word, halfStayingStillCoords, halfIncrementingCoords, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.InvalidAxis err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
         }
+
+        [TestMethod]
+        public void ShouldThrowDuplicateCoords ()
+        {
+            // setup
+            var letterTileA = new Scrabble.Game_Objects.LetterTile('A', 1);
+            var letterTileN = new Scrabble.Game_Objects.LetterTile('N', 1);
+            var letterTileT = new Scrabble.Game_Objects.LetterTile('T', 1);
+
+            List<Scrabble.Game_Objects.LetterTile> word = new List<Scrabble.Game_Objects.LetterTile> { letterTileA, letterTileN, letterTileT };
+            try
+            {
+                var play = new Scrabble.Play(word, new List<int>() { 0, 1, 0 }, new List<int>() { 0, 0, 0 }, 1);
+
+                // should error before this line.
+                Assert.IsTrue(false);
+            }
+            catch (Scrabble.Play.DuplicateCoords err)
+            {
+                Console.WriteLine(err);
+                Assert.IsTrue(true);
+            }
+            catch (Exception someOtherErr)
+            {
+                System.Diagnostics.Debug.WriteLine(someOtherErr);
+                // shouldn't throw another type of error
+                Assert.IsTrue(false);
+            }
+        }
+
+
     }
 }
